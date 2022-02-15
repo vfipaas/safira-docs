5775ca53789282e02ec2f79c8377b20eec0fbdc3
Marcello Reis <marcello.reis@finxi.com.br>
2022-02-15 18:01:50 -0300

    Ajuste no título da página

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Ipass - Safira',
+  title: 'Safira CLI',
   tagline: 'Dinosaurs are cool',
   url: ' https://vfipaas.github.io/',
   baseUrl: '/safira-docs/',
