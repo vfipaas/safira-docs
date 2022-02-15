289072f247d174db79102c25d6d9c1fa1b9f8c0e
Marcello Reis <marcello.reis@finxi.com.br>
2022-02-15 18:05:24 -0300

    Mais um ajuste de Título removendo a palavra IPaaS

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Ipass Safira',
+        title: 'Safira CLI',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
